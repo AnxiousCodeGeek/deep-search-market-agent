b838cac5f815ecea20a2650ec80d8fb37444784a
Fatima Saud <138652868+AnxiousCodeGeek@users.noreply.github.com>
2025-09-03 10:14:52 +0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -37,9 +37,9 @@ st.markdown("""
 <div style='display: flex; align-items: center; justify-content: center;'>
     <img src='https://raw.githubusercontent.com/AnxiousCodeGeek/deep-search-market-agent/main/assets/arrow.png' 
          width='50' style='margin-right: 12px;'/>
-    <div>
-        <h1 style='margin: 0; font-size: 2em;'>StoxFinlytics</h1>
-        <h4 style='margin: 2px 0 0 0; font-weight: normal; color: gray;'>Deep Search Market Agent</h4>
+    <div style='line-height: 1.2;'>
+        <h1 style='margin: 0;'>StoxFinlytics</h1>
+        <h4 style='margin: 0;'>Deep Search Market Agent</h4>
     </div>
 </div>
 """, unsafe_allow_html=True)
@@ -76,6 +76,7 @@ if user_query:
     # Show assistant response
     with st.chat_message("assistant"):
         st.write(response)
+
 
 
 
