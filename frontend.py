cbad021af4bc74efbc36dc1680ce10df85bbec40
Fatima Saud <138652868+AnxiousCodeGeek@users.noreply.github.com>
2025-09-03 10:07:46 +0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -35,7 +35,7 @@ if st.sidebar.button("🗑️ Clear All Chats"):
 # ---------- MAIN AREA ----------
 st.markdown("""
 <div style='text-align: center;'>
-    <img src='https://raw.githubusercontent.com/AnxiousCodeGeek/deep-search-market-agent/main/assets/arrow.png' width='90'/>
+    <img src='https://raw.githubusercontent.com/AnxiousCodeGeek/deep-search-market-agent/main/assets/arrow.png' width='60'/>
     <h1>StoxFinlytics</h1>
     <h4>Deep Search Market Agent</h4>
 </div>
@@ -73,4 +73,5 @@ if user_query:
     # Show assistant response
     with st.chat_message("assistant"):
         st.write(response)
+
 
